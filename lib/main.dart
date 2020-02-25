0a64f9ae5bdb60016f64fc8291c2710b50b800ae
Yoshiaki Ishikawa <yisikawa@ma.medias.ne.jp>
2020-02-25 14:12:45 +0900

    add original parts

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.grey,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.grey,
         ),
         body: DicePage(),
       ),
@@ -18,6 +18,13 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Image(
+          width: 200,
+          image: AssetImage('images/dice1.png'),
+        ),
+      ],
+    );
   }
 }
