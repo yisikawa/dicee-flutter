7b0d02afcfba817fcdcdaaa7dd5e2ba827d77ba5
Yoshiaki Ishikawa <yisikawa@ma.medias.ne.jp>
2020-02-25 18:05:30 +0900

    add flat button

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,10 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+              child: FlatButton(
+                child: Image.asset('images/dice1.png'),
+                 onPressed: () {},
+              ),
             ),
           ),
           Expanded(
